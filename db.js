b261a6f2d7a8083b04d10bc7b4b701bc0732f081
dsanchezb96 <44408826+dsanchezb96@users.noreply.github.com>
2021-04-28 02:28:18 +0200

    Create db.js

---
+++ db.js
@@ -0,0 +1,100 @@
+{
+  "fruits" : [
+    {
+      "name" : "Watermelon",
+      "image" : "https://sc04.alicdn.com/kf/U8eb4d685aae34a1680510fd0c837527ep.jpg",
+      "origin": "Egypt",
+      "family" : "Cucurbitacaeae"
+    },
+    {
+      "name" : "Mango",
+      "image" : "https://media.istockphoto.com/photos/whole-and-slice-ripe-mango-fruit-with-green-leaves-isolated-on-white-picture-id1008183290?k=6&m=1008183290&s=612x612&w=0&h=YG0jNEbUMqmcKc6lLOnChjRlqlqSnlfGuHSaeG4x7NE=",
+      "origin": "South Asia",
+      "family" : "Anacardiacaeae"
+    },
+    {
+      "name" : "Grapes",
+      "image" : "https://www.collinsdictionary.com/images/full/grape_229112122.jpg",
+      "origin" : "Georgia",
+      "family" : "Vitaceae"
+    },
+    {
+      "name" : "Pineapple",
+      "image" : "https://lehzon.com/wp-content/uploads/2020/05/pineapple-500x500-1.jpg",
+      "origin" : "South America",
+      "family" : "Bromeliaceae"
+    },
+    {
+      "name" : "Apple",
+      "image" : "https://static.libertyprim.com/files/familles/pomme-large.jpg?1569271834",
+      "origin" : "Central Asia",
+      "family" : "Rosaceae"
+    },
+    {
+      "name" : "Banana",
+      "image" : "https://static.libertyprim.com/files/familles/banane-large.jpg?1569271725",
+      "origin" : "Papua New Guinea",
+      "family" : "Musaceae"
+    },
+    {
+      "name" : "Guarana",
+      "image" : "https://www.exotic-seeds.store/8554-large_default/semillas-de-guarana.jpg",
+      "origin" : "Amazonia",
+      "family" : "Sapindaceae"
+    },
+    {
+      "name" : "Strawberry",
+      "image" : "https://static.libertyprim.com/files/familles/fraise-large.jpg?1569271765",
+      "origin" : "Alps",
+      "family" : "Rosaceae"
+    },
+    {
+      "name" : "Peach",
+      "image" : "https://static.libertyprim.com/files/familles/peche-large.jpg?1574630286",
+      "origin" : "Northwest China",
+      "family" : "Rosaceae"
+    },
+    {
+      "name" : "Pomegranate",
+      "image" : "https://avocitrus.eu/wp-content/uploads/2017/08/granada-558x600.jpg",
+      "origin" : "Spain",
+      "family" : "Punicaceae"
+    },
+    {
+      "name" : "Orange",
+      "image" : "https://5.imimg.com/data5/KM/QP/MY-22954806/orange-500x500.jpg",
+      "origin" : "Southern China",
+      "family" : "Rutaceae"
+    },
+    {
+      "name" : "Kiwi",
+      "image" : "https://images-na.ssl-images-amazon.com/images/I/41nrO6HfZsL._AC_.jpg",
+      "origin" : "China",
+      "family" : "Actinidiaceae"
+    },
+    {
+      "name" : "Sapote",
+      "image" : "https://images.heb.com/is/image/HEBGrocery/000375298?fit=constrain,1&wid=800&hei=800&fmt=jpg&qlt=85,0&resMode=sharp2&op_usm=1.75,0.3,2,0",
+      "origin" : "Mexico",
+      "family" : "Sapotaceae"
+    },
+    {
+      "name" : "Papaya",
+      "image" : "https://images-na.ssl-images-amazon.com/images/I/41%2BICmJbmVL._AC_SX425_.jpg",
+      "origin" : "Central America",
+      "family" : "Caricaceae"
+    },
+    {
+      "name" : "Avocado",
+      "image" : "https://sc04.alicdn.com/kf/H6259ee3057ae4dfeab9b84d4cc8b969f0.jpg",
+      "origin" : "Mexico",
+      "family" : "Lauraceae"
+    },
+    {
+      "name" : "Blueberries",
+      "image" : "https://www.haifa-group.com/sites/default/files/Blueberry%20isolated%2026223694_xxl.jpg",
+      "origin" : "North America",
+      "family" : "Ericaceae"
+    }
+  ]
+}
