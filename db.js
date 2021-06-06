397d93e9187cd2ee1653ae3942b9ae65cb92f240
dsanchezb96 <44408826+dsanchezb96@users.noreply.github.com>
2021-06-06 21:04:23 +0200

    Update db.js

--- db.js
+++ db.js
@@ -4,97 +4,113 @@
       "name" : "Watermelon",
       "image" : "https://www.levarht.com/wp-content/uploads/2018/05/38-watermelon-800x532.png",
       "origin": "Egypt",
-      "family" : "Cucurbitacaeae"
+      "family" : "Cucurbitacaeae",
+      "description": "Watermelon (Citrullus lanatus) is a flowering plant species of the Cucurbitaceae family. A scrambling and trailing vine-like plant, it was originally domesticated in Africa. It is a highly cultivated fruit worldwide, with more than 1,000 varieties."
     },
     {
       "name" : "Mango",
       "image" : "https://www.freshvana.com/media/t/imgs/almacen_articulo/d57de474-567e-44d7-ac4b-1200de5f5b10/mango.png.1920x1920_q85.png",
       "origin": "South Asia",
-      "family" : "Anacardiacaeae"
+      "family" : "Anacardiacaeae",
+      "description": "A mango is an edible stone fruit produced by the tropical tree Mangifera indica which is believed to have originated from the region between northwestern Myanmar, Bangladesh, and northeastern India. M. indica has been cultivated in South and Southeast Asia since ancient times resulting in two distinct types of modern mango cultivars: the "Indian type" and the "Southeast Asian type". Other species in the genus Mangifera also produce edible fruits that are also called "mangoes", the majority of which are found in the Malesian ecoregion."
     },
     {
       "name" : "Grapes",
       "image" : "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/25d45014-8cc3-4c98-b02c-5a0cf3a55ddd/ddfrpyt-7b2f2fe3-9f1c-4048-98a2-f26a877dd5de.png/v1/fill/w_900,h_667,strp/white_grapes_on_a_transparent_background__by_prussiaart_ddfrpyt-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjY3IiwicGF0aCI6IlwvZlwvMjVkNDUwMTQtOGNjMy00Yzk4LWIwMmMtNWEwY2YzYTU1ZGRkXC9kZGZycHl0LTdiMmYyZmUzLTlmMWMtNDA0OC05OGEyLWYyNmE4NzdkZDVkZS5wbmciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.z6E_mhgn3Ilm-gpUl8ObR8EpqamZR7-yAifyiPTcKqE",
       "origin" : "Georgia",
-      "family" : "Vitaceae"
+      "family" : "Vitaceae",
+      "description": "A grape is a fruit, botanically a berry, of the deciduous woody vines of the flowering plant genus Vitis. Grapes can be eaten fresh as table grapes or they can be used for making wine, jam, grape juice, jelly, grape seed extract, raisins, vinegar, and grape seed oil. Grapes are a non-climacteric type of fruit, generally occurring in clusters."
     },
     {
       "name" : "Pineapple",
       "image" : "https://www.dole.com/-/media/project/dole/produce-images/fruit/pineapple_cut_web.png?h=540&w=720&rev=77f42516c642431eac65057385324af7&hash=8A218F607D9E27339B852FB5785302A0",
       "origin" : "South America",
-      "family" : "Bromeliaceae"
+      "family" : "Bromeliaceae",
+      "description": "The pineapple (Ananas comosus) is a tropical plant with an edible fruit and the most economically significant plant in the family Bromeliaceae. The pineapple is indigenous to South America, where it has been cultivated for many centuries. The introduction of the pineapple to Europe in the 17th century made it a significant cultural icon of luxury. Since the 1820s, pineapple has been commercially grown in greenhouses and many tropical plantations. Further, it is the third most important tropical fruit in world production. In the 20th century, Hawaii was a dominant producer of pineapples, especially for the US; however, by 2016, Costa Rica, Brazil, and the Philippines accounted for nearly one-third of the world's production of pineapples."
     },
     {
       "name" : "Apple",
       "image" : "https://www.dole.com/-/media/project/dole/produce-images/fruit/apples_cut_web.png?h=540&w=720&rev=d90d9052a81f4790a2cf484fba2505fd&hash=0180255164D0EEE11A816E99EA84CE1F",
       "origin" : "Central Asia",
-      "family" : "Rosaceae"
+      "family" : "Rosaceae",
+      "description": "An apple is an edible fruit produced by an apple tree (Malus domestica). Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today. Apples have been grown for thousands of years in Asia and Europe and were brought to North America by European colonists. Apples have religious and mythological significance in many cultures, including Norse, Greek, and European Christian tradition."
     },
     {
       "name" : "Banana",
       "image" : "https://www.acai.eu/acai/CustomUpload/374O357O340O370O356O369O350O320O322O320O328O/Bananenpueree-FineFruitsClub-500x500px.gif",
       "origin" : "Papua New Guinea",
-      "family" : "Musaceae"
+      "family" : "Musaceae",
+      "description": "A banana is an elongated, edible fruit – botanically a berry – produced by several kinds of large herbaceous flowering plants in the genus Musa. In some countries, bananas used for cooking may be called "plantains", distinguishing them from dessert bananas. The fruit is variable in size, color, and firmness, but is usually elongated and curved, with soft flesh rich in starch covered with a rind, which may be green, yellow, red, purple, or brown when ripe. The fruits grow in clusters hanging from the top of the plant. Almost all modern edible seedless (parthenocarp) bananas come from two wild species – Musa acuminata and Musa balbisiana. The scientific names of most cultivated bananas are Musa acuminata, Musa balbisiana, and Musa × paradisiaca for the hybrid Musa acuminata × M. balbisiana, depending on their genomic constitution. The old scientific name for this hybrid, Musa sapientum, is no longer used."
     },
     {
       "name" : "Guarana",
       "image" : "https://mk0alovitoxq19sdr6k5.kinstacdn.com/wp-content/uploads/2020/06/quanara1.png",
       "origin" : "Amazonia",
-      "family" : "Sapindaceae"
+      "family" : "Sapindaceae",
+      "description": "Guaraná (/ɡwəˈrɑːnə/ from the Portuguese guaraná [ɡwaɾɐˈna]), Paullinia cupana, syns. P. crysan, P. sorbilis) is a climbing plant in the family Sapindaceae, native to the Amazon basin and especially common in Brazil. Guaraná has large leaves and clusters of flowers and is best known for the seeds from its fruit, which are about the size of a coffee bean."
     },
     {
       "name" : "Strawberry",
       "image" : "https://www.seriousfacts.com/wp-content/uploads/2019/03/Strawberry-is-a-member-of-rose-family-and-it-is-the-only-fruit-which-has-seeds-outside..png",
       "origin" : "Alps",
-      "family" : "Rosaceae"
+      "family" : "Rosaceae",
+      "description": "The garden strawberry (or simply strawberry; Fragaria × ananassa) is a widely grown hybrid species of the genus Fragaria, collectively known as the strawberries, which are cultivated worldwide for their fruit. The fruit is widely appreciated for its characteristic aroma, bright red color, juicy texture, and sweetness. It is consumed in large quantities, either fresh or in such prepared foods as jam, juice, pies, ice cream, milkshakes, and chocolates. Artificial strawberry flavorings and aromas are also widely used in products such as candy, soap, lip gloss, perfume, and many others."
     },
     {
       "name" : "Peach",
       "image" : "https://cdn.pixabay.com/photo/2017/07/13/10/22/fruits-2500011_960_720.png",
       "origin" : "Northwest China",
-      "family" : "Rosaceae"
+      "family" : "Rosaceae",
+      "description": "The peach (Prunus persica) is a deciduous tree native to the region of Northwest China between the Tarim Basin and the north slopes of the Kunlun Mountains, where it was first domesticated and cultivated. It bears edible juicy fruits with various characteristics, most called peaches and others (the glossy-skinned varieties), nectarines."
     },
     {
       "name" : "Pomegranate",
       "image" : "https://www.levarht.com/wp-content/uploads/2018/05/50-Pomegranate-800x559.png",
       "origin" : "Spain",
-      "family" : "Punicaceae"
+      "family" : "Punicaceae",
+      "description": "The pomegranate (Punica granatum) is a fruit-bearing deciduous shrub in the family Lythraceae, subfamily Punicoideae, that grows between 5 and 10 m (16 and 33 ft) tall. The pomegranate was originally described throughout the Mediterranean region. It was introduced into Spanish America in the late 16th century and into California by Spanish settlers in 1769."
     },
     {
       "name" : "Orange",
       "image" : "https://cdn.pixabay.com/photo/2020/04/07/06/05/navel-orange-5012172_960_720.png",
       "origin" : "Southern China",
-      "family" : "Rutaceae"
+      "family" : "Rutaceae",
+      "description": "The orange is the fruit of various citrus species in the family Rutaceae (see list of plants known as orange); it primarily refers to Citrus × sinensis, which is also called sweet orange, to distinguish it from the related Citrus × aurantium, referred to as bitter orange. The sweet orange reproduces asexually (apomixis through nucellar embryony); varieties of sweet orange arise through mutations."
     },
     {
       "name" : "Kiwi",
       "image" : "https://www.jing.fm/clipimg/full/256-2561144_fruits-luso-exotics-kiwifruit.png",
       "origin" : "China",
-      "family" : "Actinidiaceae"
+      "family" : "Actinidiaceae",
+      "description": "Kiwifruit (commonly shortened to kiwi in North America) or Chinese gooseberry is the edible berry of several species of woody vines in the genus Actinidia. The most common cultivar group of kiwifruit (Actinidia deliciosa 'Hayward') is oval, about the size of a large hen's egg: 5–8 centimetres (2–3 inches) in length and 4.5–5.5 cm (1+3⁄4–2+1⁄4 in) in diameter. It has a thin, fuzzy, fibrous, tart but edible light brown skin and light green or golden flesh with rows of tiny, black, edible seeds. The fruit has a soft texture with a sweet and unique flavour. In 2018, China produced half of the world total of kiwifruit."
     },
     {
       "name" : "Tangerine",
       "image" : "https://www.sofruco.com/wp-content/uploads/2017/05/mandarina-500x400.png",
       "origin" : "China",
-      "family" : "Rutaceae"
+      "family" : "Rutaceae",
+      "description": "The tangerine (Citrus reticulata L. var., sometimes referred as Citrus x tangerina) is a group of orange-coloured citrus fruit consisting of hybrids of mandarin orange (Citrus reticulata) with some pomelo contribution."
     },
     {
       "name" : "Papaya",
       "image" : "https://www.tuku.lk/wp-content/uploads/2020/07/papaaya.png",
       "origin" : "Central America",
-      "family" : "Caricaceae"
+      "family" : "Caricaceae",
+      "description": "The papaya (/pəˈpaɪə/, US: /pəˈpɑːjə/) (from Carib via Spanish), papaw, (/pəˈpɔː/) or pawpaw (/ˈpɔːpɔː/) is the plant Carica papaya, one of the 22 accepted species in the genus Carica of the family Caricaceae. Its origin is in the tropics of the Americas, perhaps from Central America and southern Mexico."
     },
     {
       "name" : "Avocado",
       "image" : "https://zavocado.com/wp-content/uploads/2020/07/aguacate-organico-de-exportacion-zavocado-org-aguac.png",
       "origin" : "Mexico",
-      "family" : "Lauraceae"
+      "family" : "Lauraceae",
+      "description": "The avocado (Persea americana), a tree likely originating from south-central Mexico, is classified as a member of the flowering plant family Lauraceae. The fruit of the plant, also called an avocado (or avocado pear or alligator pear), is botanically a large berry containing a single large seed. Avocado trees are partially self-pollinating, and are often propagated through grafting to maintain predictable fruit quality and quantity."
     },
     {
       "name" : "Blueberries",
       "image" : "https://www.dole.com/-/media/project/dole/produce-images/fruit/blueberries_web.png?h=540&w=720&rev=50ec31b012eb40bbba305324411a0756&hash=19AEB725333BE3F576067909B41CD408",
       "origin" : "North America",
-      "family" : "Ericaceae"
+      "family" : "Ericaceae",
+      "description": "Blueberries are perennial flowering plants with blue or purple berries. They are classified in the section Cyanococcus within the genus Vaccinium. Vaccinium also includes cranberries, bilberries, huckleberries and Madeira blueberries. Commercial blueberries—both wild (lowbush) and cultivated (highbush)—are all native to North America. The highbush varieties were introduced into Europe during the 1930s."
     }
   ]
 }
